05fd7f419cb17998d7ce2842f42019d53b06145f
Joerg Diekmann <joergd@pobox.com>
2013-03-25 08:52:55 +0200

    Added a blogging gem.

--- config.rb
+++ config.rb
@@ -1,4 +1,36 @@
 ###
+# Blog settings
+###
+
+# Time.zone = "Pretoria"
+
+activate :blog do |blog|
+  # blog.prefix = "blog"
+  blog.permalink = ":title.html"
+  # blog.sources = ":year-:month-:day-:title.html"
+  # blog.taglink = "tags/:tag.html"
+  blog.layout = "article"
+  # blog.summary_separator = /(READMORE)/
+  # blog.summary_length = 250
+  # blog.year_link = ":year.html"
+  # blog.month_link = ":year/:month.html"
+  # blog.day_link = ":year/:month/:day.html"
+  # blog.default_extension = ".markdown"
+
+  blog.tag_template = "tag.html"
+  blog.calendar_template = "calendar.html"
+
+  # blog.paginate = true
+  # blog.per_page = 10
+  # blog.page_link = "page/:num"
+end
+
+page "/feed.xml", :layout => false
+
+activate :directory_indexes
+page "/404.html", directory_index: false
+
+### 
 # Compass
 ###
 
@@ -16,13 +48,13 @@
 ###
 
 # Per-page layout changes:
-#
+# 
 # With no layout
 # page "/path/to/file.html", :layout => false
-#
+# 
 # With alternative layout
 # page "/path/to/file.html", :layout => :otherlayout
-#
+# 
 # A path which all have the same layout
 # with_layout :admin do
 #   page "/admin/*"
@@ -59,26 +91,23 @@ end
 
 # Build-specific configuration
 configure :build do
-
-  activate :asset_hash
-    
   # For example, change the Compass output style for deployment
   activate :minify_css
-
+  
   # Minify Javascript on build
-  # activate :minify_javascript
-
+  activate :minify_javascript
+  
   # Enable cache buster
   # activate :cache_buster
-
+  
   # Use relative URLs
   # activate :relative_assets
-
+  
   # Compress PNGs after build
   # First: gem install middleman-smusher
   # require "middleman-smusher"
   # activate :smusher
-
+  
   # Or use a different image path
   # set :http_path, "/Content/images/"
 end
---
+++ source/2013-03-25-good-bye-cruel-world.html.markdown
@@ -0,0 +1,7 @@
+---
+title: Good bye cruel world
+date: 2013-03-25
+tags: interesting
+---
+
+We'll be posting some stuff here about Blinkplan engineering.
